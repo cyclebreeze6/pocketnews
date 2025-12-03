f8432df2dcd9fadf06a6f28fb4779b3069524fd1
cycle breeze <cyclebreeze6@gmail.com>
2025-12-03 16:14:13 +0000

    centralize the Top menu, the category menu and the whoel website as the

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,10 +21,12 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased">
-        <FirebaseClientProvider>
-          {children}
-        </FirebaseClientProvider>
-        <Toaster />
+        <div className="max-w-7xl mx-auto">
+          <FirebaseClientProvider>
+            {children}
+          </FirebaseClientProvider>
+          <Toaster />
+        </div>
       </body>
     </html>
   );
