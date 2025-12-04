fb9c0acd65e18e89c07eeb137cb6963646b7cf53
cycle breeze <cyclebreeze6@gmail.com>
2025-12-04 23:23:01 +0000

    allow images from the youtbe image link so all the auto fetched images t

--- next.config.ts
+++ next.config.ts
@@ -33,6 +33,12 @@ const nextConfig: NextConfig = {
         hostname: 'i.ytimg.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
