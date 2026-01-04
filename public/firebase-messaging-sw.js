3b0a03b0efd8d382c5568a49f24c1214dbcb577e
cycle breeze <cyclebreeze6@gmail.com>
2026-01-04 08:46:48 +0000

    Check is the FCM notification properly configured in the backend?

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,4 +1,30 @@
+// Scripts for firebase and firebase messaging
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
 
-// A relatively empty file is required for the service worker to be picked up.
-// Firebase will configure it at runtime.
-// See: https://firebase.google.com/docs/cloud-messaging/js/client
+// Initialize the Firebase app in the service worker
+// "Default" Firebase app (important for initialization)
+firebase.initializeApp({
+    apiKey: "AIzaSyBF491cj4zNfYzX2QfvnLjvmWq3nRKHhpk",
+    authDomain: "studio-7985035708-93893.firebaseapp.com",
+    projectId: "studio-7985035708-93893",
+    storageBucket: "studio-7985035708-93893.appspot.com",
+    messagingSenderId: "56161195034",
+    appId: "1:56161195034:web:c05d8c96a170d17afbada3"
+});
+
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: payload.notification.image,
+    sound: "https://cdn.pixabay.com/audio/2022/03/15/audio_221a7a3e75.mp3"
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
